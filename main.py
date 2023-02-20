7f9f7f31fcaeadeb10a2bbe98d32779279f83dd7
Raffaele Spataro <rspataro@softstrategy.it>
2023-02-20 12:16:35 +0100

    add jinja 2 render

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import tomli
+import jinja2
 
 with open("character.toml", mode="rb") as fp:
     conf = tomli.load(fp)
 
 
-print(conf["character"])
+# jinja2 setup
+templateLoader = jinja2.FileSystemLoader(searchpath="templates")
+templateEnv = jinja2.Environment(loader=templateLoader)
+
+# sheet building
+template = templateEnv.get_template( "sheet.html" )
+# magic happens
+templateVars = conf
+
+outputText = template.render(templateVars)
+
+# print(outputText)
+
+# to save the results
+char_name = conf["character"]["name"]
+with open(f"out/{char_name}_sheet.html", "w") as fh:
+    fh.write(outputText)
