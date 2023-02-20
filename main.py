cc972abbf0abd2a17d5fe55f704ee1c2ef26de1b
Raffaele Spataro <rspataro@softstrategy.it>
2023-02-20 14:37:55 +0100

    add develop handler

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import tomli
 import jinja2
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 with open("character.toml", mode="rb") as fp:
     conf = tomli.load(fp)
+    
+class TemplateChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        # jinja2 setup
+        templateLoader = jinja2.FileSystemLoader(searchpath="templates")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+        # sheet building
+        template = templateEnv.get_template( "base.html" )
+        # magic happens
+        templateVars = conf
+
+        outputText = template.render(templateVars)
+
+        # to save the results
+        char_name = conf["character"]["name"]
+        
+        with open(f"out/{char_name} Sheet.html", "w") as fh:
+            fh.write(outputText)
 
 
-# jinja2 setup
-templateLoader = jinja2.FileSystemLoader(searchpath="templates")
-templateEnv = jinja2.Environment(loader=templateLoader)
+if __name__ == "__main__":
+    event_handler = TemplateChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path='templates', recursive=True)
+    observer.start()
 
-# sheet building
-template = templateEnv.get_template( "base.html" )
-# magic happens
-templateVars = conf
-
-outputText = template.render(templateVars)
-
-# print(outputText)
-
-# to save the results
-char_name = conf["character"]["name"]
-with open(f"out/{char_name} Sheet.html", "w") as fh:
-    fh.write(outputText)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
