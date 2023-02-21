db2de2c7e639c635b53de324a5f7b34a40438b6d
Davide Santini <d.santini@reply.it>
2023-02-21 11:37:58 +0100

    proficiency computations

--- enrich/computations.py
+++ enrich/computations.py
@@ -77,3 +77,28 @@ def get_ability_ref(ability):
     return stat_name
 
 
+def get_proficiency(level):
+    from_level_to_prof={
+        "1":	"+2",
+        "2": "+2",
+        "3": "+2",
+        "4":	"+2",
+        "5":	"+3",
+        "6":	"+3",
+        "7":	"+3",
+        "8":	"+3",
+        "9":	"+4",
+        "10":	"+4",
+        "11":	"+4",
+        "12":	"+4",
+        "13":	"+5",
+        "14":	"+5",
+        "15":	"+5",
+        "16":	"+5",
+        "17":	"+6",
+        "18":	"+6",
+        "19":	"+6",
+        "20":  "+6"}
+    
+    proficiency_mod = from_level_to_prof[level]
+    return proficiency_mod
